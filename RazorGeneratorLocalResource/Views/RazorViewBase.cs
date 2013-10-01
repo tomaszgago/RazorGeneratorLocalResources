6c3c13357093e5595d51b811b343190e75713844
Tomassoo <tomaszgago@gmail.com>
2013-10-01 10:58:23 +0200

    commit

---
+++ RazorGeneratorLocalResource/Views/RazorViewBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorGeneratorLocalResource.Views
+{
+    public abstract class RazorViewBase<T> : System.Web.Mvc.WebViewPage<T> where T : class
+    {
+        public string LocalResource(string resourceName)
+        {
+            return ViewContext.HttpContext.GetLocalResourceObject(VirtualPath, resourceName) as string;
+        }
+
+
+
+    }
+}
